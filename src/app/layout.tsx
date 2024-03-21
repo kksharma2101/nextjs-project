d81d58fb4e96d7feb036625d75419820418366a9
kamal sharma <kamalbsr2101@gmail.com>
2024-03-21 10:45:15 +0530

    add some changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" style={{ backgroundColor: "#000" }}>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,32 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
+import { cn } from "@/utils/cn";
+import Link from "next/link";
 
-export const Navbar = () => {
+export const Navbar = ({ className }: { className?: string }) => {
+    const [active, setActive] = useState<string | null>(null);
     return (
-        <div>Navbar</div>
+        <div
+            className={cn("fixed top-8 inset-x-0 max-w-2xl mx-auto z-50", className)}
+        >
+            <Menu setActive={setActive}>
+                <Link href={"/"}>
+                    <MenuItem setActive={setActive} active={active} item="Home">
+                    </MenuItem>
+                </Link>
+                <MenuItem setActive={setActive} active={active} item="Our Courses">
+                    <div className="flex flex-col space-y-4 text-sm">
+                        <HoveredLink href="/web-devlopment">Web Development</HoveredLink>
+                        <HoveredLink href="/machine-learning">Machine Learning</HoveredLink>
+                        <HoveredLink href="/data-science">Data Science</HoveredLink>
+                        <HoveredLink href="/cyber-security">Cyber Security</HoveredLink>
+                    </div>
+                </MenuItem>
+                <Link href={'/contact'}>
+                    <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
+                </Link>
+            </Menu>
+        </div>
     )
 }
