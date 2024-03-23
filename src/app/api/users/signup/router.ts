9a0564d77f42a1898b2ac2366d62c1e1515929ab
kamal sharma <kamalbsr2101@gmail.com>
2024-03-23 22:24:50 +0530

    add more functionality

--- src/app/api/users/signup/router.ts
+++ src/app/api/users/signup/router.ts
@@ -1,11 +1,46 @@
 import { NextRequest, NextResponse } from "next/server";
 import User from "@/models/userModels";
 import { connectToDb } from "@/dbConfig/dbConfig";
+import bcrypt from 'bcrypt';
+import { sendEmail } from "@/helpers/mailHelper";
+
 
 export async function POST(reqest: NextRequest) {
     try {
-        
+        const reqBody = reqest.json();
+        const { userName, email, password } = reqBody;
+
+        const user = User.findOne({ email });
+
+        if (email) {
+            return NextResponse.json({ error: "Email is already exists" }, { status: 500 })
+        }
+
+        // password bcrypt
+        const passwordSalt = await bcrypt.genSalt(8);
+        const passwordHash = await bcrypt.hash(password, passwordSalt);
+
+        const addUser = new User({
+            userName,
+            email,
+            password: passwordHash
+        })
+
+        const saveUser = await addUser.save();
+        console.log(saveUser);
+
+        // send verification email 
+        await sendEmail({ email, emailType: "VERIFIY", userId: saveUser._id });
+
+        NextResponse.json({
+            status: 404,
+            message: "User registerd successfully",
+            saveUser
+        })
+
+
+
     } catch (error) {
-       console.log("Error in signup:" + error)
+        return NextResponse.json({ error: "Something went wrong in signup" }, { status: 404 })
     }
 }
--- src/helpers/mailHelper.ts
+++ src/helpers/mailHelper.ts
@@ -1,31 +1,39 @@
+import User from "@/models/userModels";
 import nodemailer from "nodemailer";
+import bcrypt from "bcrypt";
 
-export const sendEmail = async ({email, emailType, user}:any) => {
-    try {
-        // TODO: create email sending functionality
-
-        const transporter = nodemailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // Use `true` for port 465, `false` for all other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
-
-          const emailOptions = {
-            from: 'kamalsharma.microsoft', // sender address
-            to: email, // list of receivers
-            subject: emailType == "VERIFY" ? "Verify your email" : "Reset your password", // Subject line
-            html: "<b>Hello world?</b>", // html body
-          }
-
-          const emailResponse = await transporter.sendMail(emailOptions);
-          (emailOptions)
-          return emailResponse;
-
-    } catch (error) {
-        console.log(error)
+export const sendEmail = async ({ email, emailType, userId }: any) => {
+  try {
+    const hashToken = await bcrypt.hash(userId.toString(), 8);
+    // TODO: create email sending functionality
+    if (emailType == "VERIFY") {
+      await User.findByIdAndUpdate(userId, { verifyToken: hashToken, verifyTokenExpiry: Date.now() + 3600 })
+    } else if (emailType == "RESET") {
+      await User.findByIdAndUpdate(userId, { forgotPasswordToken: hashToken, forgotPasswordTokenExpiry: Date.now() + 36000 })
     }
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.ethereal.email",
+      port: 587,
+      secure: false, // Use `true` for port 465, `false` for all other ports
+      auth: {
+        user: "maddison53@ethereal.email",
+        pass: "jn7jnAPss4f63QBp6D",
+      },
+    });
+
+    const emailOptions = {
+      from: 'kamalsharma.microsoft', // sender address
+      to: email, // list of receivers
+      subject: emailType == "VERIFY" ? "Verify your email" : "Reset your password", // Subject line
+      html: "<b>Hello world?</b>", // html body
+    }
+
+    const emailResponse = await transporter.sendMail(emailOptions);
+    (emailOptions)
+    return emailResponse;
+
+  } catch (error) {
+    console.log(error)
+  }
 }
